b19bb40a2937bb736cef56e4708ec7e455dda554
Duy <gochoctapcuatuanduy@gmail.com>
2026-05-28 22:31:31 +0700

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,37 @@ import { useState } from "react";
 // import {data} from "./data"
 import ButtonList from "./Component/ButtonList";
 import RenderBox from "./Component/RenderBox";
-import './App.css'
+import "./App.css";
+import { Analytics } from "@vercel/analytics/next";
 export default function App() {
-  const [selectedId, setSelectedId] = useState('Counter');
+  const [selectedId, setSelectedId] = useState("Counter");
   // const [active, setActive] =useState(false)
   // function handle(topic) {
   //   setSelectedId(topic)
   //   setActive(true)
   // }
+
   return (
     <>
-      <center><h1>Chuyển đổi dữ liệu thông minh</h1><p>made by github tuanduy35399</p></center>
+      <center>
+        <h1>Chuyển đổi dữ liệu thông minh</h1>
+        <p>made by github tuanduy35399</p>
+      </center>
 
-        <div className="navigation" style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-          <ButtonList Clicked={setSelectedId} isActive={selectedId}/>
-        </div>
-        <div className="layout_render">
-          <RenderBox topic={selectedId}/>
-        </div>
-        
+      <div
+        className="navigation"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ButtonList Clicked={setSelectedId} isActive={selectedId} />
+      </div>
+      <div className="layout_render">
+        <RenderBox topic={selectedId} />
+      </div>
+      <Analytics />
     </>
   );
 }
